5aeddb9785f30ab83bbc5242be87594ab0cf7ca4
charlesfeng207 <charlesfeng207@gmail.com>
2024-01-05 20:08:17 +0800

    123

--- Editor/rshell.py
+++ Editor/rshell.py
@@ -3,12 +3,16 @@ import threading
 import sys
 import time
 import json
+from enum import Enum
 
-
-DebugRShell = False
+debug_rshell = False
+class ConnectState(Enum):
+    Default = 1,
+    Connecting = 2,
+    Connected = 3,
 
 def print_debug(msg):
-    if DebugRShell:
+    if debug_rshell:
         print(msg)
 
 def receive_message(shell):
@@ -18,11 +22,12 @@ def receive_message(shell):
                 break
             data, addr = shell.client_socket.recvfrom(1024 * 1024)
             msg = json.loads(data.decode('utf-8'))
-            shell.is_connecting = False
+            shell.connect_state = ConnectState.Connected
             shell.add_to_buffer(msg)
         except Exception as e:
-            if DebugRShell:
+            if debug_rshell:
                 print("Error occurred while receiving message:", str(e))
+            shell.connect_state = ConnectState.Default
             break
 
 
@@ -58,7 +63,7 @@ class RShell(object):
         self.target_ip = "127.0.0.1"
         self.target_port = 9999
         self.on_message_received = None
-        self.is_connecting = False
+        self.connect_state = 0
         self.client_socket = None
 
         if address is not None:
@@ -77,9 +82,9 @@ class RShell(object):
         pass
 
     def init_socket(self):
-        if not self.is_connecting:
+        if self.connect_state != ConnectState.Connecting:
             print(f"RShell is connecting ... {self.target_ip}:{self.target_port}")
-            self.is_connecting = True
+            self.connect_state = ConnectState.Connecting
 
         self.waitingMsg = None
         self.msgbuffer = {}
@@ -88,21 +93,20 @@ class RShell(object):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.client_socket.sendto("hi".encode('utf-8'), (self.target_ip, self.target_port))
 
-        self.receive_thread = threading.Thread(target=receive_message, args=(self,))
+        self.receive_thread = threading.Thread(target=receive_message, name='rshell', args=(self,))
         self.receive_thread.start()
         pass
 
     def send(self, message):
         try:
+            if self.connect_state != ConnectState.Connected:
+                self.init_socket()
             self.client_socket.sendto(message.encode('utf-8'), (self.target_ip, self.target_port))    
         except Exception as e:
             print_debug("Error occurred while sending message:", str(e))
-            self.init_socket()
-            time.sleep(1)
-            self.send(message)
         pass
 
-    def sendwait(self, message, retry = -1):
+    def sendwait(self, message, retry = 10):
         self.waitingMsg = None
         self.send(message)
         start_time = time.time()
@@ -117,15 +121,18 @@ class RShell(object):
             pass
         return self.waitingMsg
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     address = sys.argv[1] if len(sys.argv) > 1 else None
+    debug_rshell = True
 
     rshell = RShell(address)
     rshell.on_message_received = lambda msg: print(msg)
-    
+
     try:
         while True:
-            message = input(f"{rshell.target_ip}:{rshell.target_port}> ")
+            message = input(f"{rshell.target_ip}:{rshell.target_port}>")
+            if message == "":
+                message = "hi"
             rshell.send(message)
             time.sleep(0.1)
             pass
@@ -133,5 +140,3 @@ if __name__ == '__main__':
         rshell.close_socket()
         print("KeyboardInterrupt")
         pass
-    
-        
